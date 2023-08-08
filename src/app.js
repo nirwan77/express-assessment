322029483e7d35422fc16067e0ce9630f2d6f3f5
Nirwan Rai <nirwanrai6969@gmail.com>
2023-08-08 11:16:35 +0545

    fixed

--- src/app.js
+++ src/app.js
@@ -12,6 +12,29 @@ app.use(cors());
 app.use(json());
 
 // TODO: add your /to-celsius and /to-fahrenheit POST handlers here:
+app.post("/to-celsius", (req, res) => {
+  const { fahrenheit } = req.body;
+  if (typeof fahrenheit !== "number") {
+    return res
+      .status(400)
+      .json({ error: "Invalid input. Fahrenheit must be a number." });
+  }
+
+  const celsius = (fahrenheit - 32) * (5 / 9);
+  return res.json({ celsius });
+});
+
+app.post("/to-fahrenheit", (req, res) => {
+  const { celsius } = req.body;
+  if (typeof celsius !== "number") {
+    return res
+      .status(400)
+      .json({ error: "Invalid input. Celsius must be a number." });
+  }
+
+  const fahrenheit = (celsius * 9) / 5 + 32;
+  return res.json({ fahrenheit });
+});
 
 app.get("/", (req, res) => {
   res.json({ message: "👋🌎🌍🌏" });
